9aa15a6fed864ddbaace643a9f93fca25df32809
Tom Kavanagh <tom.kavanagh@contino.io>
2021-10-06 10:08:45 +1100

    Removed TF infrastructure

--- meta.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  required_version = ">= 0.13.0"
-  backend "s3" {
-    bucket = "pathways-dojo"
-    key    = "tkav-tfstate-main"
-    region = "us-east-1"
-  }
-}
--- providers.tf
+++
@@ -1,4 +0,0 @@
-provider "aws" {
-  alias = "region"
-  region = "us-east-1"
-}
